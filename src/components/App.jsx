0c838b5520e2e0b227454961303439e261fd1d33
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-05-30 23:19:34 +0200

    profile-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
+import { Profile } from "./Profile/Profile";
+import user from "./Profile/user.json";
+
 function App() {
   return (
     <>
-      <h1>React Homework Template (Vite)</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}></Profile>
     </>
   );
 }
